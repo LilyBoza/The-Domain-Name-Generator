18c032495d17ac30a5f10c4cb1976e6a0a800308
LilyBoza <lily96boza@gmail.com>
2021-04-01 02:31:13 +0000

    primer intento

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  // aquí se define el evento para que al oprimir el boton se cambie la url
+  document.querySelector("#buttonURL").addEventListener("click", () => {
+    // con innerHTML logro escribir sobre el HTML para colocar la nueva frase
+    document.querySelector("#url").innerHTML = NewURL();
+  });
 };
+
+function NewURL() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dot = [".com", ".es", ".net", ".us", ".io"];
+
+  //   Aquí tengo los diferentes indices de donde voy a tomar mi url
+  let pronounIndx = Math.floor(Math.random() * pronoun.length);
+  let adjIndx = Math.floor(Math.random() * adj.length);
+  let nounIndx = Math.floor(Math.random() * noun.length);
+  let dotIndx = Math.floor(Math.random() * dot.length);
+
+  //  Me retorna el nombre de cada url distinta
+  return pronoun[pronounIndx] + adj[adjIndx] + noun[nounIndx] + dot[dotIndx];
+}
